901da860bd3be3d3307082bac3b6494b08b9d3d4
Henrik Loeser <data-henrik@users.noreply.github.com>
2017-01-12 16:56:14 +0100

    reworked initial comment section

--- bmx-sample-broker.py
+++ bmx-sample-broker.py
@@ -1,35 +1,28 @@
 ###############################################################
-# Copyright 2016 IBM Corp. All Rights Reserved.
+#
+# Copyright 2016,2017 IBM Corp
 # Author: data-henrik
 #
-# Licensed under the Apache License, Version 2.0 (the “License”);
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#  https://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an “AS IS” BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# See LICENSE file for license
 #
 ###############################################################
 #
-# Simple broker for demonstrating how to register Custom Service Brokers:
+# Simple broker for demonstrating how to register
+# Custom Service Brokers:
 # - Standard Private Broker
 # - Space-scoped Private Broker
 #
-# In the related tutorial we show how to use this broker with IBM Bluemix
-# and how to register and manage this broker in different ways.
-# The code is kept as simple as possible (from my view...).
+# In the related tutorial (README) we show how to use this broker
+# with IBM Bluemix.
+# The code is kept as simple as possible (from our view...).
 #
 # See http://docs.cloudfoundry.org/services/api.html for the
 # Cloud Foundry Broker API Documentation
 #
 # This file has two sections:
 # - Functions related to the service broker
-# - Few functions to simulate service-specific functionality like dashboard
+# - Few functions to simulate service-specific functionality
+#   like the service dashboard
 ###############################################################
 
 # Importing some necessary libraries
